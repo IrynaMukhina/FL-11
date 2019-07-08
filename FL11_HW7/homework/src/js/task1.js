19031dd439e8a8b85487c8f1e951b351eb4f617b
Iryna_M <irynam.mukhina@gmail.com>
2019-07-08 18:10:48 +0300

    initial commit

---
+++ FL11_HW7/homework/src/js/task1.js
@@ -0,0 +1,46 @@
+const userEmail = prompt(`Please enter your email`);
+if (userEmail) {
+	if (userEmail.length >= 6) {
+		if (userEmail === `user@gmail.com` || userEmail === `admin@gmail.com`) {
+			let userPassword = prompt('Please enter your password');
+			if (userPassword) {
+				if (userEmail === `user@gmail.com` && userPassword === `UserPass` ||
+				userEmail === `admin@gmail.com` && userPassword === `AdminPass`) {
+					const changePassword = confirm(`Do you want to change your password?`);
+					if (changePassword) {
+						const oldPassword = prompt(`Please enter your old password`);
+						if (userPassword === oldPassword) {
+							const newPassword = prompt(`Please enter a new password`);
+							if (newPassword.length >= 5) {
+								const repeatNewPassword = prompt(`Please repeat a new password`);
+								if (newPassword === repeatNewPassword) {
+									userPassword = newPassword;
+									alert(`You have successfully changed your password.`);
+								} else {
+									alert(`You wrote the wrong password.`);
+								}
+							} else {
+								alert(`It’s too short password. Sorry.`);
+							}
+						} else {
+							alert(`Wrong password`);
+						}
+					} else {
+						alert(`You have failed the change.`);
+					}
+				} else {
+					alert(`Wrong password`);
+				}
+			} else {
+				alert(`Canceled.`);
+			}
+		} else {
+			alert(`I don’t know you`);
+		}
+	} else {
+		alert(`I don't know any emails having name length less than 6 symbols`);
+	}
+} else {
+	alert(`Canceled.`);
+}
+
